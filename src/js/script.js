7d1805fdbf81bac6cec7c907d89a1976873b167b
Miguel <miguel.amarod37@gmail.com>
2025-06-19 11:22:49 -0300

    Add: toggleable navbar on mobile viewport

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+// Toggling navbar
+const menu = document.querySelectorAll("#menu");
+
+menu.forEach((e) => {
+  e.addEventListener("click", () => {
+    const navBar = document.querySelector("#nav_bar");
+    navBar.classList.toggle("opacity-0");
+    navBar.classList.toggle("-right-0");
+    navBar.classList.toggle("-right-full");
+  });
+});
