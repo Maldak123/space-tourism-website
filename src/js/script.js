914fe47c50a54cce70a47385285e3e23171c294a
Miguel <miguel.amarod37@gmail.com>
2025-06-21 23:31:10 -0300

    Fix: navbar correction while paginating

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Toggleable navbar
   const menu = document.querySelectorAll("#menu");
-
-  menu.forEach((e) => {
-    e.addEventListener("click", () => {
-      const navBar = document.querySelector("#nav_bar");
-      navBar.classList.toggle("opacity-0");
-      navBar.classList.toggle("-right-0");
-      navBar.classList.toggle("-right-full");
-    });
-  });
-
-  // Sections load
+  const navBar = document.querySelector("#nav_bar");
   const sections = document.querySelectorAll("#section");
   const navPages = document.querySelectorAll("#nav_item");
   const startBtn = document.querySelector("#start_btn");
   const logo = document.querySelector("#logo");
   let currentSec = 0;
 
+  // Toggleable navbar
+  function activeMenu() {
+    navBar.classList.toggle("opacity-0");
+    navBar.classList.toggle("-right-0");
+    navBar.classList.toggle("-right-full");
+  }
+
+  menu.forEach((e) => {
+    e.addEventListener("click", () => {
+      activeMenu();
+    });
+  });
+
+  // Sections load
   navPages.forEach((e) => {
     const x = [...navPages];
     e.addEventListener("click", () => {
+      activeMenu();
       changingSec(x.indexOf(e));
     });
   });
@@ -41,7 +45,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Navbar sections activation
   function changingSec(x) {
-    if (currentSec == x) {
+    if (currentSec === x) {
       return;
     } else {
       sections[currentSec].classList.toggle("hidden");
